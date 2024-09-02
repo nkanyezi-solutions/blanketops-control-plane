d7cc9237c3dd6ff21e2ef36482e586bc7cb7d06c
ntlaletsi70 <ntlaletsi86@gmail.com>
2024-09-02 20:53:44 +0200

    Tailor # Halt these tests for now

--- certificate_authority/gen_all.sh
+++ certificate_authority/gen_all.sh
@@ -2,10 +2,14 @@
 
 bash gen_ca.sh
 bash gen_admin.sh
+
 bash gen_kube_controller_manager.sh
 bash gen_kube_proxy.sh
+
 bash gen_kube_scheduler.sh
-bash gen_kube_apiserver.sh
-bash gen_apiserver_kubelet_client.sh
-bash gen_etcd.sh
-bash gen_service_account.sh
+
+# bash gen_kube_apiserver.sh
+
+# bash gen_apiserver_kubelet_client.sh
+# bash gen_etcd.sh
+# bash gen_service_account.sh
--- etcd/verification.sh
+++ etcd/verification.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-ETCDCTL_API=etcdctl member list --endpoints=https://127.0.0.1:2379 --cacert=/etc/etcd/ca.crt --cert=/etc/etcd/etcd-server.crt --key=/etc/etcd/etcd-server.key
+ETCDCTL_API=3 etcdctl member list \
+  --endpoints=https://127.0.0.1:2379 \
+  --cacert=/etc/etcd/ca.crt \
+  --cert=/etc/etcd/etcd-server.crt \
+  --key=/etc/etcd/etcd-server.key
