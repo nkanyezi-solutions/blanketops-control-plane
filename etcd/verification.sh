71fc44f926b76a2d0592d29b7bffda6e283df40c
ntlaletsi70 <ntlaletsi86@gmail.com>
2024-09-02 20:45:27 +0200

    Tailor # to move for gen certs

--- etcd/verification.sh
+++ etcd/verification.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-sudo ETCDCTL_API=3 etcdctl member list \
-  --endpoints=https://127.0.0.1:2379 \
-  --cacert=/etc/etcd/ca.crt \
-  --cert=/etc/etcd/etcd-server.crt \
-  --key=/etc/etcd/etcd-server.key
+ETCDCTL_API=etcdctl member list --endpoints=https://127.0.0.1:2379 --cacert=/etc/etcd/ca.crt --cert=/etc/etcd/etcd-server.crt --key=/etc/etcd/etcd-server.key
