138d1775c3e6ad0ed7aa721342a2e369c8257f18
ntlaletsi70 <ntlaletsi86@gmail.com>
2024-09-02 15:32:09 +0200

    Tailor # Etcd dependencies

---
+++ etcd/verification.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo ETCDCTL_API=3 etcdctl member list \
+  --endpoints=https://127.0.0.1:2379 \
+  --cacert=/etc/etcd/ca.crt \
+  --cert=/etc/etcd/etcd-server.crt \
+  --key=/etc/etcd/etcd-server.key
